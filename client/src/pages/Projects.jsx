b632077335fa104f77a2983035616ec40b61aec9
Goodluck Japhet <goodluckjaphet7@gmail.com>
2024-03-21 19:02:39 +0300

    Fix typo in Projects page heading

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -5,7 +5,7 @@ import CallToAction_2 from "../components/CallToAction_2";
 export default function Projects() {
   return (
     <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
-      <h1 className="text-3xl font-semibold">Pojects</h1>
+      <h1 className="text-3xl font-semibold">Projects</h1>
       <p className="text-md text-gray-500">
       Uni Media has a myriad of exciting projects and events lined up for you to explore and engage with.
       </p>
